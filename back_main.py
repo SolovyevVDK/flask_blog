042171b76e113716cb38e40133054bdf4a3fccf8
Yury <y.s.solovyev@yandex.ru>
2023-04-26 20:53:00 +1000

    correct

--- back_main.py
+++ back_main.py
@@ -4,15 +4,11 @@ from datetime import datetime, timedelta
 import requests
 from bs4 import BeautifulSoup
 
-from functions import auth, all_write, logout, clean_base, path_to_txt, read_category_price, read_products_id, \
-    read_stocks, read_suppliers, read_category, read_products_name, category_pricelist, timedata, invoice_by_number, \
-    all_invoices
-from variables import protocol, server, port, bd
+import functions
 import hashlib
-
+server_data = functions.read_server_data()
 login = 'admin'  # Со страницы авторизации
 password = 'restoRtest'  # Со страницы авторизации
-token = auth(login, password)  # Функция получения токена, написана в файле
-
-print(token.text)
-logout(token)
+token = functions.auth(login, password, server_data)  # Функция получения токена, написана в файле
+print(functions.write_suppliers(token))
+functions.logout(token, server_data)
--- functions.py
+++ functions.py
@@ -39,7 +39,10 @@ def auth(login, password, server_data):
 
 # Закрытие токена
 def logout(token, server_data):
-    out_url = (protocol + '://' + server + ':' + port + bd + '/api/logout?key=' + token.text)
+    out_url = (server_data['protocol'] + '://' +
+               server_data['server'] + ':' +
+               server_data['port'] +
+               server_data['bd'] + '/api/logout?key=' + token.text)
     requests.get(out_url)
 
 
@@ -72,75 +75,111 @@ def clean_base():
 
 
 # Записать В ЛОГ склады
-def write_stocks(token):
-    gets_url = (protocol + '://' + server + ':' + port + bd + '/api/corporation/stores?key=' + token.text)
+def write_stocks(token, server_data):
+    gets_url = (server_data['protocol'] + '://' +
+                server_data['server'] + ':' +
+                server_data['port'] +
+                server_data['bd'] + '/api/corporation/stores?key=' + token.text)
     store = requests.get(gets_url)
     soup = BeautifulSoup(store.content, 'xml')
-    stocks_list = []
-    for name in soup.find_all('name'):
-        stocks_list.append(name.string)
-    stocks_id = []
-    for id in soup.find_all('id'):
-        stocks_id.append(id.text)
-    stocks_full = dict(zip(stocks_list, stocks_id))
-    with open(Path(path_to_txt, 'stocks.txt'), 'w', encoding=encod) as file:
-        json.dump(stocks_full, file, indent=4, ensure_ascii=False)
-    return
+    stores = []
+    for corporateItemDto in soup.find_all('corporateItemDto'):
+        store = {
+            "id": corporateItemDto.find('id').string,
+            "parentId": corporateItemDto.find('parentId').string,
+            "code": corporateItemDto.find('code').string,
+            "name": corporateItemDto.find('name').string,
+            "type": corporateItemDto.find('type').string
+        }
+        stores.append(store)
+    with open(Path(path_to_txt, 'stocks.json'), 'w', encoding=encod) as file:
+        json.dump(stores, file, indent=4, ensure_ascii=False)
+    return stores
 
 
 # Прочитать ИЗ ЛОГА и записать в переменную склады
 def read_stocks():
-    with open(Path(path_to_txt, 'stocks.txt'), encoding=encod) as file:
+    with open(Path(path_to_txt, 'stocks.json'), encoding=encod) as file:
         stocks = json.load(file)
     return stocks
 
 
-# Записываем список продуктов В ЛОГ {name:id}
-def write_products_name(token):
-    url = (protocol + '://' + server + ':' + port + bd + '/api/products?key=' + token.text)
+def products(token, server_data):
+    url = (server_data['protocol'] + '://' +
+           server_data['server'] + ':' +
+           server_data['port'] +
+           server_data['bd'] + '/api/products?key=' + token.text)
     response = requests.get(url)
     soup = BeautifulSoup(response.content, 'xml')
-    products_list = []
-    for name in soup.find_all('name'):
-        products_list.append(name.string)
-    products_id = []
-    for id in soup.find_all('id'):
-        products_id.append(id.text)
-    products_full = dict(zip(products_list, products_id))
-    with open(Path(path_to_txt, 'products_name.txt'), 'w', encoding=encod) as file:
-        json.dump(products_full, file, indent=4, ensure_ascii=False)
-    return products_full
+    result = []
+    for productDto in soup.find_all('productDto'):
+        try:
+            id = productDto.find('id').string
+        except AttributeError:
+            id = 'None'
+
+        try:
+            parentId = productDto.find('parentId').string
+        except AttributeError:
+            parentId = 'None'
+
+        try:
+            num = productDto.find('num').string
+        except AttributeError:
+            num = 'None'
+
+        try:
+            code = productDto.find('code').string
+        except AttributeError:
+            code = 'None'
+
+        try:
+            name = productDto.find('name').string
+        except AttributeError:
+            name = 'None'
+
+        try:
+            productType = productDto.find('productType').string
+        except AttributeError:
+            productType = 'None'
+
+        try:
+            cookingPlaceType = productDto.find('cookingPlaceType').string
+        except AttributeError:
+            cookingPlaceType = 'None'
+
+        try:
+            mainUnit = productDto.find('mainUnit').string
+        except AttributeError:
+            mainUnit = 'None'
+
+        try:
+            productCategory = productDto.find('productCategory').string
+        except AttributeError:
+            productCategory = 'None'
+
+        eta = {
+            "id": id,
+            "parentId": parentId,
+            "num": num,
+            "code": code,
+            "name": name,
+            "productType": productType,
+            "cookingPlaceType": cookingPlaceType,
+            "mainUnit": mainUnit,
+            "productCategory": productCategory
+        }
+        result.append(eta)
+    with open(Path(path_to_txt, 'products.json'), 'w', encoding=encod) as file:
+        json.dump(result, file, indent=4, ensure_ascii=False)
+    return result
 
 
 # Прочитать ИЗ ЛОГА продукты {name:id}
-def read_products_name():
-    with open(Path(path_to_txt, 'products_name.txt'), encoding=encod) as file:
-        products_name = json.load(file)
-    return products_name
-
-
-# Записать список продуктов В ЛОГ {id:name}
-def write_products_id(token):
-    url = (protocol + '://' + server + ':' + port + bd + '/api/products?key=' + token.text)
-    response = requests.get(url)
-    soup = BeautifulSoup(response.content, 'xml')
-    products_list = []
-    for name in soup.find_all('name'):
-        products_list.append(name.string)
-    products_id = []
-    for id in soup.find_all('id'):
-        products_id.append(id.text)
-    products_full = dict(zip(products_id, products_list))
-    with open(Path(path_to_txt, 'products_id.txt'), 'w', encoding=encod) as file:
-        json.dump(products_full, file, indent=4, ensure_ascii=False)
-    return products_full
-
-
-# Прочитать ИЗ ЛОГА {id:name} продукты
-def read_products_id():
-    with open(Path(path_to_txt, 'products_id.txt'), encoding=encod) as file:
-        products_id = json.load(file)
-    return products_id
+def read_products():
+    with open(Path(path_to_txt, 'products.json'), encoding=encod) as file:
+        products_new = json.load(file)
+    return products_new
 
 
 # Запись В ЛОГ словарь контрагентов (название : id)
@@ -148,16 +187,133 @@ def write_suppliers(token):
     url = (protocol + '://' + server + ':' + port + bd + '/api/suppliers?key=' + token.text)
     response = requests.get(url)
     soup = BeautifulSoup(response.content, 'xml')
-    suppliers_list = []
-    for name in soup.find_all('name'):
-        suppliers_list.append(name.string)
-    suppliers_id = []
-    for id in soup.find_all('id'):
-        suppliers_id.append(id.text)
-    suppliers_full = dict(zip(suppliers_list, suppliers_id))
-    with open(Path(path_to_txt, 'suppliers.txt'), 'w', encoding=encod) as file:
-        json.dump(suppliers_full, file, indent=4, ensure_ascii=False)
-    return suppliers_full
+    # suppliers_list = []
+    # for name in soup.find_all('name'):
+    #     suppliers_list.append(name.string)
+    # suppliers_id = []
+    # for id in soup.find_all('id'):
+    #     suppliers_id.append(id.text)
+    # suppliers_full = dict(zip(suppliers_list, suppliers_id))
+    # with open(Path(path_to_txt, 'suppliers.txt'), 'w', encoding=encod) as file:
+    #     json.dump(suppliers_full, file, indent=4, ensure_ascii=False)
+    suppliers = []
+    for employees in soup.find_all('employee'):
+        try:
+            id = employees.find('id').string
+        except AttributeError:
+            id = 'None'
+        try:
+            code = employees.find('code').string
+        except AttributeError:
+            code = 'None'
+        try:
+            name = employees.find('name').string
+        except AttributeError:
+            name = 'None'
+        try:
+            login = employees.find('login').string
+        except AttributeError:
+            login = 'None'
+        try:
+            phone = employees.find('phone').string
+        except AttributeError:
+            phone = 'None'
+        try:
+            cellPhone = employees.find('cellPhone').string
+        except AttributeError:
+            cellPhone = 'None'
+        try:
+            firstName = employees.find('firstName').string
+        except AttributeError:
+            firstName = 'None'
+        try:
+            middleName = employees.find('middleName').string
+        except AttributeError:
+            middleName = 'None'
+        try:
+            lastName = employees.find('lastName').string
+        except AttributeError:
+            lastName = 'None'
+        try:
+            birthday = employees.find('birthday').string
+        except AttributeError:
+            birthday = 'None'
+        try:
+            email = employees.find('email').string
+        except AttributeError:
+            email = 'None'
+        try:
+            address = employees.find('address').string
+        except AttributeError:
+            address = 'None'
+        try:
+            cardNumber = employees.find('cardNumber').string
+        except AttributeError:
+            cardNumber = 'None'
+        try:
+            taxpayerIdNumber = employees.find('taxpayerIdNumber').string
+        except AttributeError:
+            taxpayerIdNumber = 'None'
+        try:
+            snils = employees.find('snils').string
+        except AttributeError:
+            snils = 'None'
+        try:
+            preferredDepartmentCode = employees.find('preferredDepartmentCode').string
+        except AttributeError:
+            preferredDepartmentCode = 'None'
+        try:
+            departmentCodes = employees.find('departmentCodes').string
+        except AttributeError:
+            departmentCodes = 'None'
+        try:
+            responsibilityDepartmentCodes = employees.find('responsibilityDepartmentCodes').string
+        except AttributeError:
+            responsibilityDepartmentCodes = 'None'
+        try:
+            deleted = employees.find('deleted').string
+        except AttributeError:
+            deleted = 'None'
+        try:
+            supplier = employees.find('supplier').string
+        except AttributeError:
+            supplier = 'None'
+        try:
+            client = employees.find('client').string
+        except AttributeError:
+            client = 'None'
+        try:
+            representsStore = employees.find('representsStore').string
+        except AttributeError:
+            representsStore = 'None'
+        employeer = {
+            "id": id,
+            "code": code,
+            "name": name,
+            "login": login,
+            "phone": phone,
+            "cellPhone": cellPhone,
+            "firstName": firstName,
+            "middleName": middleName,
+            "lastName": lastName,
+            "birthday": birthday,
+            "email": email,
+            "address": address,
+            "cardNumber": cardNumber,
+            "taxpayerIdNumber": taxpayerIdNumber,
+            "snils": snils,
+            "preferredDepartmentCode": preferredDepartmentCode,
+            "departmentCodes": departmentCodes,
+            "responsibilityDepartmentCodes": responsibilityDepartmentCodes,
+            "deleted": deleted,
+            "supplier": supplier,
+            "client": client,
+            "representsStore": representsStore
+        }
+        suppliers.append(employeer)
+    with open(Path(path_to_txt, 'suppliers.json'), 'w', encoding=encod) as file:
+        json.dump(suppliers, file, indent=4, ensure_ascii=False)
+    return suppliers
 
 
 # Прочитать ИЗ ЛОГА словарь поставщиков
